c81571da41578894067a211973b468f0fff8ce01
Ivan Chavero <ichavero@redhat.com>
2017-10-14 15:45:31 -0600

    Add better char management

--- users/genpass.pl
+++ users/genpass.pl
@@ -2,8 +2,12 @@
 
 use strict;
 srand(time ^ ($$+($$<<15)) ^ unpack "%32L*",`ps -a | /usr/bin/gzip -c -f` );
-my $MAXTAM=9;
-my @chars=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9);
+my $MAXTAM = 9;
+my @chars = ('a'..'z', 0..9, 'A'..'Z', '@', '!', '#', '$');
+
+if ($ARGV[0] != "") {
+    $MAXTAM = $ARGV[0]; 
+}
 
 print genpass($MAXTAM);
 
