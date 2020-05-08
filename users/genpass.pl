8fbf0d1917729b81888dd62e0321a3ff651095b2
Ivan Chavero <ichavero@chavero.com.mx>
2020-05-08 15:32:19 -0600

    Add more chars to password generator

--- users/genpass.pl
+++ users/genpass.pl
@@ -1,21 +1,25 @@
 #!/usr/bin/perl
 
 use strict;
-srand(time ^ ($$+($$<<15)) ^ unpack "%32L*",`ps -a | /usr/bin/gzip -c -f` );
-my $MAXTAM = 9;
-my @chars = ('a'..'z', 0..9, 'A'..'Z', '@', '!', '#', '$');
 
-if ($ARGV[0] != "") {
-    $MAXTAM = $ARGV[0]; 
+my $MAX_SIZE = 9;
+if($#ARGV >= 0) {
+    $MAX_SIZE = int($ARGV[0]);
 }
 
-print genpass($MAXTAM);
+srand(time ^ ($$+($$<<15)) ^ unpack "%32L*",`ps -a | /bin/gzip -c -f` );
+
+# Store the chars in an array to avoid using the char() function
+my @chars = ('!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', 0..9, ':', ';', '<', '=', '>', '?', '@', 'A'..'Z', '[', ']', '_', 'a'..'z');
+
+print genpass($MAX_SIZE);
 
 sub genpass {
-	my($tam)=@_;
-	my $pass;
-	for(my $i=0;$i<=$tam;$i++){
-		$pass=$pass.$chars[rand($#chars+1)];
-	}
-	return($pass);
+    my($size) = @_;
+    my $pass;
+    for(my $i = 0; $i <= $size; $i++){
+        $pass = $pass.$chars[rand($#chars+1)];
+    }
+    return($pass);
 }
+
